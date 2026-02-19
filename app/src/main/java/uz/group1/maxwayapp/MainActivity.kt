a55a3dc07834e6bc5f4c0915ff13c56e9728fc98
IkromjonovMarufjon <ikromjonovmaruf@gmail.com>
2026-02-19 09:23:20 +0500

    finish register task

--- app/src/main/java/uz/group1/maxwayapp/MainActivity.kt
+++ app/src/main/java/uz/group1/maxwayapp/MainActivity.kt
@@ -17,4 +17,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun testMethod() {
+
+    }
 }
+
+
+/*
+1)  O'zingizni branchingiz commit
+2)  o'zingizni branchingizda update project qilasiz (bu bitta branchda o'zingiz ishlasangiz optional) [merge chiqsa fix qilasiz]
+3)  push qilasiz
+4) Master/main checkout
+4) Master/main  asosiy branchga o'tib [UNDA KOD YOZMAYSIZ] faqat update project qilasiz
+5) Ozingizni branch checkout
+6) Merge main/master into "ozingizni branch" agar conflick chiqsa hal qilasiz
+7)  ozingizni branchga push qilasiz
+8) Githubga kirib pull reqeust yaratasiz
+9) Trello zadacha statae update qilib pull request link qo';shasiz. pull reqeust haqida guruhda ham ogohlantirib qo'yasiz
+* */
