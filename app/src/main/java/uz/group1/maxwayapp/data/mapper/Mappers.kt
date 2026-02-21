0c80ff77d1a312935f9ff1011131e31a0debc244
Umarxon <murodhonovuz@gmail.com>
2026-02-21 17:57:27 +0500

    add[mapper API->UI]

--- app/src/main/java/uz/group1/maxwayapp/data/mapper/Mappers.kt
+++ app/src/main/java/uz/group1/maxwayapp/data/mapper/Mappers.kt
@@ -1,10 +1,14 @@
 package uz.group1.maxwayapp.data.mapper
 
+import uz.group1.maxwayapp.data.model.NotificationUiData
 import uz.group1.maxwayapp.data.model.StoryUIData
+import uz.group1.maxwayapp.data.sources.remote.response.NotificationData
 import uz.group1.maxwayapp.data.sources.remote.response.StoryData
 
 fun StoryData.toUiData(): StoryUIData{
-
     return StoryUIData(id, url,name)
+}
 
+fun NotificationData.toUiData(): NotificationUiData{
+    return NotificationUiData(id, imgURL, message, name, sendDate)
 }
