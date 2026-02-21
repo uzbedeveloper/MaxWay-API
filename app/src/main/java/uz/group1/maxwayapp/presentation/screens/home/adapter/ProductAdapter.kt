c900b0448428d820d24d5f72cadb4ab2c1983d60
adilkhan <kenesovadilxan786@gmail.com>
2026-02-22 00:52:09 +0500

    feat: implement product list adapter and UI layout

---
+++ app/src/main/java/uz/group1/maxwayapp/presentation/screens/home/adapter/ProductAdapter.kt
@@ -0,0 +1,55 @@
+package uz.group1.maxwayapp.presentation.screens.main.banner.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import uz.group1.maxwayapp.data.model.ProductUIData
+import uz.group1.maxwayapp.databinding.ItemProductBinding
+
+class ProductAdapter : ListAdapter<ProductUIData, ProductAdapter.ProductViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder(ItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ProductViewHolder(private val binding: ItemProductBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(pr: ProductUIData){
+            binding.textName.text = pr.name
+            binding.textCost.text = pr.cost.toString()
+
+
+            Glide.with(binding.imgProduct.context)
+                .load(pr.image)
+                .centerCrop()
+                .into(binding.imgProduct)
+
+
+            binding.cardCost.setOnClickListener {
+                binding.cardCost.visibility = View.GONE
+                binding.countContainer.visibility = View.VISIBLE
+
+            }
+        }
+    }
+
+    companion object{
+        val DiffCallback = object : DiffUtil.ItemCallback<ProductUIData>(){
+            override fun areItemsTheSame(oldItem: ProductUIData, newItem: ProductUIData): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ProductUIData, newItem: ProductUIData): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
