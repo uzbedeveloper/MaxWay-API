13d3bb9333a473841bb1d6cf1e44a4a2326827b6
Umarxon <murodhonovuz@gmail.com>
2026-02-22 17:34:37 +0500

    add[Stories Circle on MainPage]

--- app/src/main/java/uz/group1/maxwayapp/presentation/screens/nointernet/NoConnectionFragment.kt
+++ app/src/main/java/uz/group1/maxwayapp/presentation/screens/nointernet/NoConnectionFragment.kt
@@ -1,16 +1,37 @@
 package uz.group1.maxwayapp.presentation.screens.nointernet
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import uz.gita.leeson_network.utils.NetworkMonitor
 import uz.group1.maxwayapp.R
+import uz.group1.maxwayapp.databinding.ScreenNoConnectionBinding
+import uz.group1.maxwayapp.utils.NotificationType
+import uz.group1.maxwayapp.utils.showNotification
 
 class NoConnectionFragment: Fragment(R.layout.screen_no_connection) {
+
+    private val binding by viewBinding(ScreenNoConnectionBinding::bind)
+    private lateinit var networkMonitor: NetworkMonitor
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        networkMonitor = NetworkMonitor(requireContext().applicationContext)
 
+        binding.btnRetry.setOnClickListener {
+            checkConnection()
+        }
     }
 
+    private fun checkConnection() {
+        if (networkMonitor.isConnected()) {
+            findNavController().popBackStack()
+        } else {
+            requireActivity().showNotification("Internet yo'q", NotificationType.ERROR)
+        }
+    }
 }
--- app/src/main/java/uz/group1/maxwayapp/presentation/screens/stories/StoriesViewModelFactory.kt
+++ app/src/main/java/uz/group1/maxwayapp/presentation/screens/stories/StoriesViewModelFactory.kt
@@ -3,13 +3,13 @@ package uz.group1.maxwayapp.presentation.screens.stories
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import uz.group1.maxwayapp.data.repository_impl.StoryRepositoryImpl
-import uz.group1.maxwayapp.domain.usecase.impl.GetStoriesUseCase
+import uz.group1.maxwayapp.domain.usecase.impl.GetStoriesUseCaseImpl
 
 class StoriesViewModelFactory: ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
 
         val repo = StoryRepositoryImpl.getInstance()
-        val getStoriesUseCase = GetStoriesUseCase(repo)
+        val getStoriesUseCase = GetStoriesUseCaseImpl(repo)
 
         return StoriesViewModelImpl(getStoriesUseCase) as T
     }
