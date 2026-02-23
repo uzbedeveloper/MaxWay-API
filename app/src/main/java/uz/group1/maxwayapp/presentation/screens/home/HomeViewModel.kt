75f3b737e4b876f6920531d9b7b987edec682dc4
Umarxon <murodhonovuz@gmail.com>
2026-02-23 14:07:56 +0500

    fix[Deleting comments]

--- app/src/main/java/uz/group1/maxwayapp/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/uz/group1/maxwayapp/presentation/screens/home/HomeViewModel.kt
@@ -1,11 +1,6 @@
 package uz.group1.maxwayapp.presentation.screens.home
 
-import androidx.lifecycle.LiveData
 import kotlinx.coroutines.flow.StateFlow
-import uz.group1.maxwayapp.data.model.BannerUIData
-import uz.group1.maxwayapp.data.model.CategoryChipUI
-import uz.group1.maxwayapp.data.model.CategoryUIData
-import uz.group1.maxwayapp.data.model.StoryUIData
 import uz.group1.maxwayapp.domain.HomeUiElements
 
 interface HomeViewModel {
--- app/src/main/java/uz/group1/maxwayapp/presentation/screens/home/HomeViewModelImpl.kt
+++ app/src/main/java/uz/group1/maxwayapp/presentation/screens/home/HomeViewModelImpl.kt
@@ -1,23 +1,12 @@
 package uz.group1.maxwayapp.presentation.screens.home
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import uz.group1.maxwayapp.data.model.BannerUIData
-import uz.group1.maxwayapp.data.model.CategoryChipUI
-import uz.group1.maxwayapp.data.model.CategoryUIData
-import uz.group1.maxwayapp.data.model.StoryUIData
 import uz.group1.maxwayapp.domain.HomeUiElements
 import uz.group1.maxwayapp.domain.usecase.BannerUseCase
 import uz.group1.maxwayapp.domain.usecase.GetCategoriesUseCase
